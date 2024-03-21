287244bbf7101e2d0024c7d9e1f4c3175da0fd83
crocs-in-socks <gouriswag1@gmail.com>
2024-03-21 23:14:42 +0530

    Added logout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,9 +5,23 @@ import { useNavigate } from 'react-router-dom'
 
 function Home() {
 
+    const navigate = useNavigate()
+    const handleLogout = async () => {
+        try {
+          const response = await axios.post('/logout')
+          if(response.status === 200) {
+            navigate('/login')
+          }
+        }
+        catch(error) {
+          console.log(error)
+        }
+    }
+
     return (
         <div>
             Home
+            <a className="logout-button" onClick={handleLogout}>Logout</a>
         </div>
   )
 }
